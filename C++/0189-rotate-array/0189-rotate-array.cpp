8cda73954ba1ea25609b1d22b7f5472bf8d1fca1
mohdAsif03hy <mohdasif70568@gmail.com>
2026-07-18 19:43:59 +0530

    0189-rotate-array Medium .cpp 07-18-2026

--- C++/0189-rotate-array/0189-rotate-array.cpp
+++ C++/0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
+
+    // Reverse function without STL
+    void reverseArray(vector<int>& nums, int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k)
+    {
         int n = nums.size();
-        k = k%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-        
+
+        // Handle cases where k > n
+        k = k % n;
+
+        // Step 1: Reverse the entire array
+        reverseArray(nums, 0, n - 1);
+
+        // Step 2: Reverse first k elements
+        reverseArray(nums, 0, k - 1);
+
+        // Step 3: Reverse remaining elements
+        reverseArray(nums, k, n - 1);
     }
 };
