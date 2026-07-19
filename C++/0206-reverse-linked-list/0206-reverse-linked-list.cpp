679058b5f74fe6a9a26e38eb6f21ce1ee7d48d17
mohdAsif03hy <mohdasif70568@gmail.com>
2026-07-19 20:47:18 +0530

    0206-reverse-linked-list Easy .cpp 07-19-2026

--- C++/0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ C++/0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-
         ListNode* curr = head;
-        ListNode* prev = nullptr;
+        ListNode* prev = NULL;
 
-        while(curr != nullptr)
-        {
-            ListNode* next = curr->next; // 1️⃣ store
-            curr->next = prev;           // 2️⃣ reverse
-            prev = curr;                // 3️⃣ shift prev
-            curr = next;                // 4️⃣ shift curr
+        while (curr != NULL) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
 
         return prev;
