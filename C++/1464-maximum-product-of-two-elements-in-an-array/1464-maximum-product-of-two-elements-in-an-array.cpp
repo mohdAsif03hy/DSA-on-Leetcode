94b146a61f2578334d9dd8291995aaf887638a48
Moh Asif <mohdasif70568@gmail.com>
2026-07-27 19:32:25 +0530

    Optimize maxProduct function to find two largest elements

--- C++/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ C++/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return ((nums[nums.size()-2])-1) * ((nums[nums.size()-1])-1);
+        int max =0;
+        int indx = 0;
+        for(int i =0 ;i<nums.size();i++){
+            if(nums[i]>max){
+                max = nums[i];
+                indx = i;
+            }
+        }
+        int smax = INT_MIN;
+        for(int i =0 ;i<nums.size();i++){
+            if(i == indx){
+                continue;
+            }
+            if(smax < nums[i]){
+                smax = nums[i];
+            }
+        }
+        return (smax -1 ) * (max -1);
     }
 };
