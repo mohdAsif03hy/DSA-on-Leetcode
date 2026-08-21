01dca3889e48a6afb5869c86ff69fcdb773a7c69
Moh Asif <mohdasif70568@gmail.com>
2026-08-21 15:48:56 +0530

    0680-valid-palindrome-ii Easy .cpp 08-21-2026Two Pointers

---
+++ C++/0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPal(string s, int i , int j){
+        while(i<j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s[i] == s[j]) {
+                i++;
+                j--;
+            } else {
+                return isPal(s,i+1,j) || isPal(s,i,j-1);
+            }
+        }
+        return true;
+    }
+};
